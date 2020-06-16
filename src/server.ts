cb3b69e1fdea623539dd8c44638da1af1f5d7658
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-17 01:46:18 +0530

    load .env config while running server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import app from './app'
-import http from 'http'
-import logger from './logging/logger'
+import dotenv from "dotenv";
+import app from "./app";
+import http from "http";
+import logger from "./logging/logger";
+dotenv.config();
 
-const PORT: number = Number(process.env.PORT) || 8080
+const PORT: number = Number(process.env.PORT) || 8080;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, (): void => {
-  logger.info(`started server on port ${PORT}`)
-})
+  logger.info(`started server on port ${PORT}`);
+});
